73522239ad80cc2a326ab58078d9c6c174c36f6f
Alexader <tyxj58@gmail.com>
2021-01-26 11:26:38 +0800

    fix(cmd): enable plugin name config

--- cmd/pier/start.go
+++ cmd/pier/start.go
@@ -12,9 +12,8 @@ import (
 	"time"
 
 	"github.com/meshplus/bitxhub-kit/log"
-	"github.com/meshplus/pier/internal/loggers"
-
 	"github.com/meshplus/pier/internal/app"
+	"github.com/meshplus/pier/internal/loggers"
 	"github.com/meshplus/pier/internal/repo"
 	"github.com/urfave/cli"
 )
@@ -25,7 +24,6 @@ var (
 		Usage:  "Start a long-running daemon process",
 		Action: start,
 	}
-	pluginName = "appchain_plugin"
 )
 
 func start(ctx *cli.Context) error {
@@ -64,7 +62,7 @@ func start(ctx *cli.Context) error {
 	case repo.RelayMode:
 		fallthrough
 	case repo.DirectMode:
-		if err := checkPlugin(); err != nil {
+		if err := checkPlugin(config.Appchain.Plugin); err != nil {
 			return fmt.Errorf("check plugin: %w", err)
 		}
 
@@ -125,7 +123,7 @@ func runPProf(port int64) {
 	}()
 }
 
-func checkPlugin() error {
+func checkPlugin(pluginName string) error {
 	// check if plugin exists
 	pluginRoot, err := repo.PluginPath()
 	if err != nil {
@@ -136,7 +134,7 @@ func checkPlugin() error {
 	_, err = os.Stat(pluginPath)
 	if err != nil {
 		if os.IsNotExist(err) {
-			return fmt.Errorf("plugin file not exist")
+			return fmt.Errorf("plugin file `%s` is required", pluginPath)
 		}
 
 		return fmt.Errorf("get plugin file state error: %w", err)
