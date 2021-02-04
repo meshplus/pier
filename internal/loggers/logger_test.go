f3511124bf6c30dbd56425c9af4e2582f043eccf
jzhe <jz570889446@gmail.com>
2021-02-04 13:54:30 +0800

    test:add logger&repo unit tests

---
+++ internal/loggers/logger_test.go
@@ -0,0 +1,35 @@
+package loggers
+
+import (
+	"github.com/meshplus/pier/internal/repo"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestLogger(t *testing.T) {
+	config := &repo.Config{
+		Log: repo.Log{
+			Dir:          "logs",
+			Filename:     "pier.log",
+			ReportCaller: false,
+			Level:        "info",
+			Module: repo.LogModule{
+				AppchainMgr: "info",
+				Exchanger:   "info",
+				Executor:    "info",
+				BxhLite:     "info",
+				Monitor:     "info",
+				Swarm:       "info",
+				RuleMgr:     "info",
+				Syncer:      "info",
+				PeerMgr:     "info",
+				Router:      "info",
+				ApiServer:   "info",
+			},
+		},
+	}
+	InitializeLogger(config)
+	Logger(ApiServer).Info("api_server")
+	exchangerLoggerLevel := w.loggers[Exchanger].Logger.Level.String()
+	require.Equal(t, config.Log.Module.Exchanger, exchangerLoggerLevel)
+}
