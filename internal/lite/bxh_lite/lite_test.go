fe55d8795317df2264f2ecf178fbcda1a96fd682
dawn-to-dusk <1927529251@qq.com>
2021-02-23 19:24:08 +0800

    test(lite): add test for lite module

--- internal/lite/bxh_lite/lite_test.go
+++ internal/lite/bxh_lite/lite_test.go
@@ -2,6 +2,7 @@ package bxh_lite
 
 import (
 	"context"
+	"fmt"
 	"io/ioutil"
 	"testing"
 	"time"
@@ -69,6 +70,96 @@ func TestSyncHeader(t *testing.T) {
 	done <- true
 	require.Equal(t, uint64(2), lite.height)
 	require.Nil(t, lite.Stop())
+
+	// query nonexistent header
+	_, err = lite.QueryHeader(10)
+	require.NotNil(t, err)
+	// query unmarshal error header
+	lite.storage.Put(headerKey(20), []byte("a"))
+	_, err = lite.QueryHeader(20)
+	require.NotNil(t, err)
+
+	// start with get last height error
+	lite.storage.Put(headerHeightKey(), []byte("a"))
+	err = lite.Start()
+	require.NotNil(t, err)
+}
+
+func TestSyncHeader_Start_GetChainMateError(t *testing.T) {
+	lite, client, _ := prepare(t)
+	defer lite.storage.Close()
+
+	client.EXPECT().GetChainMeta().Return(nil, fmt.Errorf("get chain meta error")).AnyTimes()
+	err := lite.Start()
+	require.NotNil(t, err)
+}
+
+func TestSyncHeader_Recover_GetBlockHeaderError(t *testing.T) {
+	lite, client, _ := prepare(t)
+	defer lite.storage.Close()
+
+	// expect mock module returns
+	txs := make([]*pb.Transaction, 0, 2)
+	txs = append(txs, getTx(t), getTx(t))
+	h1 := getBlockHeader(t, txs, 1)
+
+	syncHeaderCh := make(chan interface{}, 2)
+	meta := &pb.ChainMeta{
+		Height:    1,
+		BlockHash: types.NewHashByStr(from),
+	}
+
+	client.EXPECT().Subscribe(gomock.Any(), pb.SubscriptionRequest_BLOCK_HEADER, gomock.Any()).Return(syncHeaderCh, nil).AnyTimes()
+	client.EXPECT().GetChainMeta().Return(meta, nil).AnyTimes()
+	client.EXPECT().GetBlockHeader(gomock.Any(), gomock.Any(), gomock.Any(), gomock.Any()).Do(
+		func(ctx context.Context, begin, end uint64, ch chan<- *pb.BlockHeader) {
+			ch <- h1
+			close(ch)
+		}).Return(fmt.Errorf("get block header error")).AnyTimes()
+
+	err := lite.Start()
+	require.Nil(t, err)
+}
+
+func TestBxhLite_HandleBlockHeaderError(t *testing.T) {
+	lite, _, _ := prepare(t)
+	defer lite.storage.Close()
+
+	txs := make([]*pb.Transaction, 0, 2)
+	txs = append(txs, getTx(t), getTx(t))
+	h1 := getBlockHeader(t, txs, 1)
+
+	lite.height = 2
+	// handle nil header
+	lite.handleBlockHeader(nil)
+	require.Equal(t, uint64(2), lite.height)
+	// handle header with number less than demand height
+	lite.handleBlockHeader(h1)
+	require.Equal(t, uint64(2), lite.height)
+}
+
+func TestBxhLite_GetHeaderChannelError(t *testing.T) {
+	lite, client, _ := prepare(t)
+	defer lite.storage.Close()
+
+	syncHeaderCh := make(chan interface{}, 0)
+
+	client.EXPECT().Subscribe(gomock.Any(), pb.SubscriptionRequest_BLOCK_HEADER, gomock.Any()).Return(nil, fmt.Errorf("subscribe error")).Times(1)
+	client.EXPECT().Subscribe(gomock.Any(), pb.SubscriptionRequest_BLOCK_HEADER, gomock.Any()).Return(syncHeaderCh, nil).AnyTimes()
+
+	go func() {
+		lite.getHeaderChannel()
+	}()
+}
+
+func TestBxhLite_SyncBlockHeaderError(t *testing.T) {
+	lite, client, _ := prepare(t)
+	defer lite.storage.Close()
+
+	client.EXPECT().Subscribe(gomock.Any(), pb.SubscriptionRequest_BLOCK_HEADER, gomock.Any()).Return(nil, fmt.Errorf("subscribe error")).AnyTimes()
+
+	ch := make(chan *pb.BlockHeader, maxChSize)
+	require.NotNil(t, lite.syncBlockHeader(ch))
 }
 
 func prepare(t *testing.T) (*BxhLite, *mock_client.MockClient, []crypto.PrivateKey) {
