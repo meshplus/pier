6de32626c49a20a14877d0e50f36c095211cdfc7
xiaochenl <nhdxlxc@163.com>
2021-05-11 14:06:55 +0800

    ci(*):add auto release docker image scripts
    1. add auto release docker image scripts
    2. modify the release package naming format

--- scripts/docker_entrypoint.sh
+++ scripts/docker_entrypoint.sh
@@ -4,8 +4,21 @@ set -e
 APPCHAIN_NAME=$1
 PLUGIN_CONFIG=$2
 
-pier --repo=/root/.pier appchain register --name=${APPCHAIN_NAME} --type=fabric --validators=/root/.pier/${PLUGIN_CONFIG}/fabric.validators --desc="appchain for test" --version=1.4.1
-pier --repo=/root/.pier rule deploy --path=/root/.pier/validating.wasm
+pier --repo=/root/.pier appchain method register --admin-key ./key.json --method fabappchain \
+     --doc-addr /ipfs/QmQVxzUqN2Yv2UHUQXYwH8dSNkM8ReJ9qPqwJsf8zzoNUi \
+     --doc-hash QmQVxzUqN2Yv2UHUQXYwH8dSNkM8ReJ9qPqwJsf8zzoNUi \
+     --name "${APPCHAIN_NAME}" --type fabric --desc="test for fabric" --version v1.4.3 \
+     --validators ./"${PLUGIN_CONFIG}"/fabric.validators --consensus raft
+
+
+command1=$(pier --repo=/root/.pier rule deploy --path ./"${PLUGIN_CONFIG}"/validating.wasm --method fabappchain --admin-key ./key.json)
+address=$(echo "$command1"|grep -o '0x.\{40\}')
+echo "${address}"
+
+command2=$(pier --repo=/root/.pier rule bind --addr "${address}" --method fabappchain --admin-key ./key.json)
+proposalID=$(echo "$command2"|grep -o '0x.\{42\}')
+echo "${proposalID}"
+
 pier --repo=/root/.pier start
 
 exec "$@"
