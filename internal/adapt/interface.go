5b3d392e8463eff21aa882d0efb2bfcd19fb0605
DiyiWi <809290733@qq.com>
2021-09-13 10:28:27 +0800

    feat(*):start relay mode reconstruc

---
+++ internal/adapt/interface.go
@@ -0,0 +1,26 @@
+package adapt
+
+import "github.com/meshplus/bitxhub-model/pb"
+
+//go:generate mockgen -destination mock_adapt/mock_adapt.go -package mock_adapt -source interface.go
+type Adapt interface {
+	// Start starts adapt
+	Start() error
+	// Stop stops adapt
+	Stop() error
+
+	// MonitorIBTP listen on ibtp from dest chain
+	MonitorIBTP() chan *pb.IBTP
+
+	// QueryIBTP query ibtp by id and type, contain mutilsign
+	QueryIBTP(id string, typ pb.IBTP_Type) *pb.IBTP
+
+	// SendIBTP check and send ibtp to dest chain
+	SendIBTP(ibtp *pb.IBTP) error
+
+	// GetServiceIDList getServiceIDList from dest chain
+	GetServiceIDList() []string
+
+	// QueryInterchain  queryInterchain from dest chain
+	QueryInterchain(serviceID string) *pb.Interchain
+}
