0e40fdf622ea1772da733984401db6ef85717946
jiuhuche120 <760297795@qq.com>
2023-01-30 11:36:35 +0800

    feat(*): fix golangci-lint action

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -3,6 +3,10 @@
 BLUE='\033[0;34m'
 NC='\033[0m'
 
+function print_blue() {
+  printf "${BLUE}%s${NC}\n" "$1"
+}
+
 function go_install() {
   version=$(go env GOVERSION)
   if [[ ! "$version" < "go1.16" ]];then
@@ -12,10 +16,6 @@ function go_install() {
   fi
 }
 
-function print_blue() {
-  printf "${BLUE}%s${NC}\n" "$1"
-}
-
 print_blue "===> 1. Install packr2"
 if ! type packr2 >/dev/null 2>&1; then
   go_install github.com/gobuffalo/packr/v2/packr2@v2.8.3
@@ -23,13 +23,13 @@ fi
 
 print_blue "===> 2. Install golangci-lint"
 if ! type golanci-lint >/dev/null 2>&1; then
-  go get github.com/golangci/golangci-lint/cmd/golangci-lint@v1.23.0
+  go_install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.23.0
 fi
 
 print_blue "===> 3. Install go mock tool"
 if ! type gomock >/dev/null 2>&1; then
-  go get github.com/golang/mock/gomock
+  go_install github.com/golang/mock/gomock
 fi
 if ! type mockgen >/dev/null 2>&1; then
-  go get github.com/golang/mock/mockgen
+  go_install github.com/golang/mock/mockgen
 fi
