c61371e89488557b865e26f6ad6e6f3dfa85177d
Ruoxin <liruoxin@hyperchain.cn>
2023-04-04 15:10:10 +0800

    fix: update packr2

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -7,20 +7,34 @@ function print_blue() {
   printf "${BLUE}%s${NC}\n" "$1"
 }
 
-print_blue "===> 1. Install packr"
-if ! type packr >/dev/null 2>&1; then
-  go get -u github.com/gobuffalo/packr/packr
+function go_install() {
+  version=$(go env GOVERSION)
+  if [[ ! "$version" < "go1.16" ]];then
+      go install "$@"
+  else
+      go get "$@"
+  fi
+}
+
+print_blue "===> 1. Install packr2"
+if ! type packr2 >/dev/null 2>&1; then
+  go_install github.com/gobuffalo/packr/v2/packr2@v2.8.3
 fi
 
 print_blue "===> 2. Install golangci-lint"
 if ! type golanci-lint >/dev/null 2>&1; then
-  go get github.com/golangci/golangci-lint/cmd/golangci-lint@v1.23.0
+    version=$(go env GOVERSION)
+    if [[ ! "$version" < "go1.16" ]];then
+        go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.50.1
+    else
+        go get github.com/golangci/golangci-lint/cmd/golangci-lint@v1.23.0
+    fi
 fi
 
 print_blue "===> 3. Install go mock tool"
 if ! type gomock >/dev/null 2>&1; then
-  go get github.com/golang/mock/gomock
+  go_install github.com/golang/mock/gomock
 fi
 if ! type mockgen >/dev/null 2>&1; then
-  go get github.com/golang/mock/mockgen
+  go_install github.com/golang/mock/mockgen
 fi
